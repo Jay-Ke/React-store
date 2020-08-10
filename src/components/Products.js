00202b46942ff75268b76ac1d4f65555ecaeea76
Canto <jk5643@nyu.edu>
2020-08-09 21:54:55 -0700

    Setup product page layout. Setup toolbox and product component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,32 @@
 import React from "react";
+import ToolBox from "components/ToolBox";
+import Product from "components/Product";
 
 class Products extends React.Component {
 	render() {
 		return (
-			<div className="products">
-				<p>Products component</p>
+			<div>
+				{/* Product page consist of toolbox componend and product compoenet */}
+				<ToolBox />
+				<div className="products">
+					<div className="columns is-multiline is-desktop">
+						<div className="column is-3">
+							<Product />
+						</div>
+                        <div className="column is-3">
+							<Product />
+						</div>
+                        <div className="column is-3">
+							<Product />
+						</div>
+                        <div className="column is-3">
+							<Product />
+						</div>
+                        <div className="column is-3">
+							<Product />
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
