163b9de3bd327d772497b0ee8b1a95a7a6d1d70f
Canto <jk5643@nyu.edu>
2020-08-10 22:00:20 -0700

    Add dynamic render for product

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,6 +3,56 @@ import ToolBox from "components/ToolBox";
 import Product from "components/Product";
 
 class Products extends React.Component {
+	// product = {
+	// 	name: "Air Jordan 4",
+	// 	image: "images/1.jpg",
+	// 	tags: "45 colors",
+	// 	price: "59440",
+	// 	status: "available",
+	// };
+	products = [
+		{
+			id: 1,
+			name: "Air Jordan 4",
+			image: "images/1.jpg",
+			tags: "45 colors",
+			price: "59440",
+			status: "available",
+		},
+		{
+			id: 2,
+			name: "Air Jordan 4",
+			image: "images/2.jpg",
+			tags: "45 colors",
+			price: "59440",
+			status: "available",
+		},
+		{
+			id: 3,
+			name: "Air Jordan 4",
+			image: "images/3.jpg",
+			tags: "45 colors",
+			price: "59440",
+			status: "available",
+		},
+		{
+			id: 4,
+			name: "Air Jordan 4",
+			image: "images/4.jpg",
+			tags: "45 colors",
+			price: "59440",
+			status: "available",
+		},
+		{
+			id: 5,
+			name: "Air Jordan 4",
+			image: "images/5.jpg",
+			tags: "45 colors",
+			price: "59440",
+			status: "available",
+		},
+	];
+
 	render() {
 		return (
 			<div>
@@ -10,21 +60,28 @@ class Products extends React.Component {
 				<ToolBox />
 				<div className="products">
 					<div className="columns is-multiline is-desktop">
+						{this.products.map((p) => {
+							return (
+								<div className="column is-3" key={p.id}>
+									<Product product={p} />
+								</div>
+							)
+						})}
+						{/* <div className="column is-3">
+							<Product product={this.product} />
+						</div>
 						<div className="column is-3">
-							<Product />
+							<Product product={this.product} />
 						</div>
-                        <div className="column is-3">
-							<Product />
+						<div className="column is-3">
+							<Product product={this.product} />
 						</div>
-                        <div className="column is-3">
-							<Product />
+						<div className="column is-3">
+							<Product product={this.product} />
 						</div>
-                        <div className="column is-3">
-							<Product />
-						</div>
-                        <div className="column is-3">
-							<Product />
-						</div>
+						<div className="column is-3">
+							<Product product={this.product} />
+						</div> */}
 					</div>
 				</div>
 			</div>
