b8ae49393e1a8ba768541925af97eddb55d000f0
Canto <jk5643@nyu.edu>
2020-08-16 21:43:20 -0700

    Render cart component and cart item component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Layout from "Layout";
+import CartItem from "./CartItem";
 
 const Cart = () => (
 	<Layout>
 		<div className="cart-page">
-			<p className="title has-text-centered">Cart Parge</p>
+			<span className="cart-title">Shopping Cart</span>
+            <div className="cart-list">
+                <CartItem />
+                <CartItem />
+                <CartItem />
+            </div>
+            <div className="cart-total">
+                Total:
+                <span className="total-price">$1234</span>
+            </div>
 		</div>
 	</Layout>
 );
